63690d3c8348a286cdd92e9226de4966a8346f29
Yzubi <42348748+Yzubi@users.noreply.github.com>
2019-01-30 13:41:23 -0200

    Update simpleflipnormals.py

--- simpleflipnormals.py
+++ simpleflipnormals.py
@@ -1,5 +1,6 @@
 import bpy
 for bpy.context.scene.objects.active in bpy.context.selected_objects:
     bpy.ops.object.editmode_toggle()
+    bpy.ops.mesh.select_all(action='SELECT')
     bpy.ops.mesh.flip_normals()
     bpy.ops.object.mode_set()
